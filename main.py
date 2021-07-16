c6135c5db57b022c11c280b3d47f469d0f07d2b0
Yuanxing Zhang <1179442802@qq.com>
2021-07-16 14:33:31 +0800

    process data, add three columns

--- main.py
+++ main.py
@@ -19,11 +19,27 @@ def clean_df(df):
     return df.dropna(how='all', axis=1)  # Drop Column Only if All the Values are Null
 
 
+def get_dict(map_df):
+    simple_dict = map_df.set_index("UNIT_NAME")["UnitGrpSimple"].to_dict()
+    return simple_dict
+
+
+def processed_df(turnaround_df, simple_dict):  # add additional three column
+    # df = clean_df(turnaround_df)
+    df = turnaround_df.copy()
+    df['UnitGrpSimple'] = df['UNIT_NAME'].map(simple_dict)
+    df['SMN'] = pd.to_datetime(df['START_DATE']).dt.strftime('%Y%m')
+    df['EMN'] = pd.to_datetime(df['END_DATE']).dt.strftime('%Y%m')
+    return df
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    pass
     turnaround_df = pd.read_csv(f"offlineEvents-2021-07-15.csv")
     unit_map = pd.read_excel(f"iir_name_mapping.xlsx", sheet_name="IirUnit_NameMirror")
-    map = unit_map[['UNIT_NAME','UnitGrpSimple']]
-    dict = map.set_index("UNIT_NAME")["UnitGrpSimple"].to_dict()
+    simple_dict = get_dict(unit_map)
+    df = processed_df(turnaround_df, simple_dict)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
